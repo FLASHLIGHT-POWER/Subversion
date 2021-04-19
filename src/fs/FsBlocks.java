e9c0c8f63766728df6c8f3cf1e2fe1ccc20eb64c
FLASHLIGHT-POWER <1602798557@qq.com>
2021-04-19 22:35:00 +0800

    0

--- src/fs/FsBlocks.java
+++ src/fs/FsBlocks.java
@@ -5,6 +5,9 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.content.*;
 
+import static mindustry.type.ItemStack.*;
+import static mindustry.Vars.*;
+
 import fs.type.*;
 
 public class FsBlocks implements ContentList{
