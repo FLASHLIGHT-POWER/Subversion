c1725d1f671f25920805afa350c68c202c992194
FLASHLIGHT-POWER <1602798557@qq.com>
2021-04-19 22:27:55 +0800

    0

--- src/fs/FsBlocks.java
+++ src/fs/FsBlocks.java
@@ -1,6 +1,7 @@
 package fs;
 
 import mindustry.ctype.*;
+import mindustry.type.*;
 import mindustry.world.*;
 
 import fs.type.*;
@@ -14,6 +15,9 @@ public class FsBlocks implements ContentList{
 	public void load(){
 		robotBase = new InsideBlock("RobotBase",3){{
 			peopleMax = 0;
+			size = 3;
+			requirements(Category.effect, with(Items.copper, 50, Items.lead, 90, Items.graphite, 70));
+			health = 300;
 		}};
 	}
 }
