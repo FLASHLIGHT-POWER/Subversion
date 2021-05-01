aaa34d6507eea8cc49174ef89797af6f0c6fb1b1
FLASHLIGHT-POWER <1602798557@qq.com>
2021-05-01 13:15:02 +0800

    0

--- src/fs/type/InsideBlock.java
+++ src/fs/type/InsideBlock.java
@@ -1,6 +1,17 @@
 package fs.type;
 
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.struct.*;
+import arc.util.*;
+import arc.util.io.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.gen.*;
+import mindustry.type.*;
 import mindustry.world.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
 
 public class InsideBlock extends Block{
 	
